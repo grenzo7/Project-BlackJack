3406b72c3f849249016a531f3616affad4fbd348
Utsav Ghimire <102249506+grenzo7@users.noreply.github.com>
2022-11-24 21:05:01 -0600

    Update blackjack_helper.py

--- Project_BlackJack_Part5_Multiplayer/blackjack_helper.py
+++ Project_BlackJack_Part5_Multiplayer/blackjack_helper.py
@@ -1,92 +1,95 @@
 from random import randint
 
+
+def starter(players):
+  scores_dict = {}
+  for i in range(1,players+1):
+    player_i = input(f"What is player {i}'s name? ")
+    scores_dict[player_i] = [0,3]
+  return scores_dict
+
 def print_card_name(card_rank):
-    # Implement card_name function here
-    if card_rank == 1:
-    # A 1 stands for an ace.
-        card_name = "Ace"
-    elif card_rank == 11:
-    # An 11 stands for a jack.
-        card_name = "Jack"
-    elif card_rank == 12:
-    # A 12 stands for a queen.
-        card_name = "Queen"
-    elif card_rank == 13:
-    # A 13 stands for a king.
-        card_name = "King"
-    else:
-    # All other cards are named by their
-    # number, or rank.
-        card_name = str(card_rank)
+  if card_rank == 1:
+    card_name = 'Ace'
+  elif card_rank == 11:
+    card_name = 'Jack'
+  elif card_rank == 12:
+    card_name = 'Queen'
+  elif card_rank == 13:
+    card_name = 'King'
+  else:
+    card_name = card_rank
 
-    if card_rank == 1 or card_rank == 8:
-        drew_prefix = 'Drew an '
-    else:
-        drew_prefix = 'Drew a '
+  if card_rank == 8 or card_rank == 1:
+    print('Drew an ' + str(card_name))
+  elif card_rank < 1 or card_rank > 13:
+    print('BAD CARD')
+  else:
+    print('Drew a ' + str(card_name))
 
-    if card_rank < 1 or card_rank > 13:
-        print('BAD CARD')
-    else:
-        print(drew_prefix + card_name)
 
 def draw_card():
-   
-    cardvalue_dict={1:"Ace",2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,11:"Jack",12:"Queen",13:"King"}
-    card=randint(1,13)
-   
-    print_card_name(card)
-    if card==11 or card==12 or card==13:
-        card=10
-        return card
-    elif card==1:
-        card=11
-        return card
-    else:
-        return card
+  card_rank = randint(1, 13)
+  print_card_name(card_rank)
+
+  if card_rank == 11 or card_rank == 12 or card_rank == 13:
+    card_value = 10
+  elif card_rank == 1:
+    card_value = 11
+  else:
+    card_value = card_rank
+
+  return card_value
+
 
 def print_header(message):
-    
-    print("-----------")
-    print(str(message) )
-    print("-----------")   
+  print('-----------')
+  print(message)
+  print('-----------')
 
 
 def draw_starting_hand(name):
-    
-    print_header(name+" TURN")
-    card1=draw_card()
-    card2=draw_card()
-    hand_value=card1+card2
-    return hand_value
-
-def print_end_turn_status(hand_value):
-    
-    print("Final hand: {}.".format(hand_value) )
-    if hand_value==21:
-        print("BLACKJACK!")
-    elif hand_value>21:
-        print("BUST.")
+  print_header(name + ' TURN')
+  return draw_card() + draw_card()
 
 
-def print_end_game_status(user_hand, dealer_hand):
-  
-    print_header("GAME RESULT")
-    if user_hand<21:
-        if dealer_hand<21:
-            if user_hand<dealer_hand:
-                print("Dealer wins!")
-            elif user_hand>dealer_hand:
-                print("You win!")
-            else:
-                print("Push.")
-        elif dealer_hand == 21:
-            print("Dealer wins!")
-        elif dealer_hand >21:
-            print("You win!")
-    elif user_hand>21:
-        print("Dealer wins!")
-    elif user_hand==21:
-        if dealer_hand ==21:
-            print("Push.")
-        else:
-            print("You win!")
+def print_end_turn_status(player,player_hand,scores_dict):
+  print('Final hand: ' + str(player_hand) + '.')
+
+  if player_hand == 21:
+    print('BLACKJACK!')
+  elif player_hand > 21:
+    print('BUST.')
+  scores_dict[player][0] = player_hand
+  return scores_dict 
+
+def print_end_game_status(player_hand, dealer_hand,scores_dict):
+  print_header('GAME RESULT')
+  updated_dict = {}
+  for player in scores_dict:
+    if player == "Dealer":
+      continue
+    if scores_dict[player][0] > scores_dict["Dealer"][0] and scores_dict[player][0] <= 21:
+      scores_dict[player][1] += 1
+      print(f"{player} wins! Score : {scores_dict[player][1]}")
+      
+    elif (scores_dict[player][0] >= scores_dict["Dealer"][0] or scores_dict[player][0] <= scores_dict["Dealer"][0])  and scores_dict[player][0] > 21:
+      scores_dict[player][1] -= 1
+      print(f"{player} loses! Score: {scores_dict[player][1]}")
+      
+    elif scores_dict[player][0] < scores_dict["Dealer"][0] and scores_dict["Dealer"][0] <= 21:
+      scores_dict[player][1] -= 1
+      print(f"{player} loses! Score: {scores_dict[player][1]}")
+
+    elif scores_dict[player][0] < scores_dict["Dealer"][0] and scores_dict["Dealer"][0] > 21:
+      scores_dict[player][1] += 1
+      print(f"{player} wins! Score : {scores_dict[player][1]}")
+    elif scores_dict[player][0] == scores_dict["Dealer"][0]:
+      print(f"{player} pushes! Score : {scores_dict[player][1]}")
+    if scores_dict[player][1] <= 0:
+      print(f"{player} eliminated!")
+    else:
+      updated_dict[player] = [0,scores_dict[player][1]]
+  if updated_dict == {}:
+    print("All players eliminated!")
+  return updated_dict
